c184f565560d3744c5902ca9a9f2bf27c59ccf90
FETHI SIMED <simedfethi@gmail.com>
2021-11-14 23:13:39 +0100

    auturization and roles

---
+++ tests/BlogPostTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Component\Panther\PantherTestCase;
+
+class BlogPostTest extends PantherTestCase
+{
+    public function testSomething(): void
+    {
+        $client = static::createPantherClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertSelectorTextContains('h1', 'Hello World');
+    }
+}
+
